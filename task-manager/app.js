6e6984213a91fdb4676a5a68c075b92719a02950
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-10-23 16:44:50 +0200

    Added an error handler middleware

--- task-manager/app.js
+++ task-manager/app.js
@@ -4,15 +4,16 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/db')
 require('dotenv').config()
 const notFound = require('./middleware/notFound')
+const errorHandlerMiddleware = require('./middleware/errorHandler')
 
 // Middleware
 app.use(express.json())
 app.use(express.static('./public'))
 
-
 // routes
 app.use('/api/v1/tasks', tasks)
 app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const port = 3000
 const start = async () => {
