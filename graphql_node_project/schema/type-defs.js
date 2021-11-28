bbce40fe1297f56957ad0a8163efe9bcb0d627d2
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-11-28 07:39:29 +0100

    Added mutations to type definations and resolvers - Mutations such as: 1. createUser 2. Update username 3. delete user

--- graphql_node_project/schema/type-defs.js
+++ graphql_node_project/schema/type-defs.js
@@ -16,12 +16,27 @@ const typeDefs = gql`
         yearOfRelease: Int!
         isInCinemas: Boolean!
     }
-
     type Query {
         users: [User!]! 
         user(id: ID!): User
         movies: [Movie!]!
         movie(name: String!): Movie
+    }
+
+    input CreateUserInput {
+        name: String!
+        username: String!
+        age: Int!
+        nationality: Nationality = NIGERIA
+    }
+    input UpdateUsernameInput {
+        id: ID!
+        newUsername: String!
+    }
+    type Mutation {
+        createUser(input: CreateUserInput!): User
+        updateUsername(input: UpdateUsernameInput!): User
+        deleteUser(id: ID!): User
     }
 
     enum Nationality {
